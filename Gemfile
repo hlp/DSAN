6f2b20c5626018e60894c182f0def94bcbb44b00
Patrick Tierney <patrick.l.tierney@gmail.com>
2011-07-11 18:25:14 -0400

    Cleanup up user profile page and added profile photo

--- Gemfile
+++ Gemfile
@@ -16,5 +16,6 @@ end
 group :test do
 	gem 'rspec'
 	gem 'webrat'
+	gem 'factory_girl_rails'
 end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+
+  def image_for(user, image_size = 100)
+    image_tag("default_profile_large.jpeg", :alt => user.name,
+              :class => "profile_image", 
+              :size => "#{image_size}x#{image_size}")
+  end
+
 end
