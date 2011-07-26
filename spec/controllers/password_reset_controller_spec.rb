f2ba9668662a5789aa23e93d545a7c4b5c36bd4e
Patrick Tierney <patrick.l.tierney@gmail.com>
2011-07-26 07:06:01 -0400

    Updated password_reset_controller spec so that it properly tests everything.

--- spec/controllers/password_reset_controller_spec.rb
+++ spec/controllers/password_reset_controller_spec.rb
@@ -1,19 +1,29 @@
 require 'spec_helper'
 
 describe PasswordResetController do
+  render_views
 
   describe "GET 'new'" do
+
     it "should be successful" do
       get 'new'
       response.should be_success
     end
-  end
 
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
+  end # GET new
+
+  describe "POST 'create'" do
+    
+    before(:each) do
+      @user = Factory(:user, :email => "patrick.l.tierney+rspec@gmail.com")
+      @attr = { :email => "patrick.l.tierney+rspec@gmail.com" }
     end
-  end
+    
+    it "should redirect to the root page" do
+      post :create, :password_reset => @attr
+      response.should redirect_to(root_path)
+    end
+
+  end # POST create
 
 end
